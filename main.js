a59f11de4515af53c93131dc344de2b04b61ef39
anhdungle <anhdungle634@gmail.com>
2020-05-07 23:32:37 +0200

    add mining

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const SH256 = require('crypto-js/sha256')
+const SHA256 = require('crypto-js/sha256')
 class Block {
   constructor(index, timestamp, data, previousHash = '') {
     this.index = index;
@@ -6,16 +6,26 @@ class Block {
     this.data = data;
     this.previousHash = previousHash;
     this.hash = this.calculateHash();
+    this.nonce = 0;
   }
 
   calculateHash(){
-    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+  }
+
+  mineBlock(difficulty){
+    while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+      this.nonce++;
+      this.hash = this.calculateHash()
+    }
+    console.log("Block mined: " + this.hash)
   }
 }
 
 class Blockchain{
   constructor(){
     this.chain = [this.createGenesisBlock];
+    this.difficulty = 2;
   }
 
   createGenesisBlock(){
@@ -28,8 +38,11 @@ class Blockchain{
 
   addBlock(newBlock){
     newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.hash = newBlock.calculateHash();
+    newBlock.mineBlock(this.difficulty);
     this.chain.push(newBlock);
   }
 }
 
+let myCoin = new Blockchain;
+console.log("Adding 1st block ...")
+myCoin.addBlock(new Block(1, "07.05.2020", "First block", ""))
